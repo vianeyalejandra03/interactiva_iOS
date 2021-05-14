99d008f4dc676043d6f5d53f10ad4ad64eee2632
FlutLab.io <>
2021-05-14 20:54:05 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -25,29 +25,102 @@ class Principal extends StatelessWidget {
         centerTitle: true,
         title: Text('Registro De Mascotas'),
       ), //fin de appbar
-      body: Container(
-        margin: EdgeInsets.only(
-          top: 10.0,
-          left: 10.0,
-          right: 10.0,
-        ),
-        height: 200,
-        width: 350,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          image: DecorationImage(image: NetworkImage("https://raw.githubusercontent.com/vianeyalejandra03/mis_imagenes/main/veterinaria1.jpg"), alignment: Alignment.topCenter),
-          border: Border(
-            top: BorderSide(
-              color: Colors.white,
-              width: 5.0,
-            ),
-            bottom: BorderSide(
-              color: Colors.white,
-              width: 5.0,
-            ),
-          ),
-        ), //fin de el decoration box
-      ), //container fin
+      body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Container(
+            //zona verde
+            color: Colors.greenAccent[400],
+            width: 1000,
+            height: 570,
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                //contenedor dentro del children
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.orange,
+                  width: 500,
+                  height: 75,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.red,
+                        width: 60,
+                        height: 55,
+                      ), //fin container rojo
+                      SizedBox(width: 16),
+                      Container(
+                        color: Colors.blue,
+                        width: 60,
+                        height: 55,
+                      ), //fin container azul
+                      SizedBox(width: 16),
+                      Container(
+                        color: Colors.green,
+                        width: 60,
+                        height: 55,
+                      ), //fin container verde
+                    ], //fin del widget[]
+                  ), //fin del row fin de fila 1
+                ), //fin del container naranja
+
+                SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.teal,
+                  width: 500,
+                  height: 75,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(color: Colors.green, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.blue, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.red, width: 60, height: 55),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.orange,
+                  width: 500,
+                  height: 75,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(color: Colors.red, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.blue, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.green, width: 60, height: 55),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.orange,
+                  width: 500,
+                  height: 75,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(color: Colors.green, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.blue, width: 60, height: 55),
+                      SizedBox(width: 16),
+                      Container(color: Colors.red, width: 60, height: 55),
+                    ],
+                  ),
+                ),
+              ], //fin del children widget de column widget[]
+            ), //fin de column
+          ) //fin de container
+          ), //fin de padding
     ); //fin de scaffold
   } //widget fin
 } //fin class principal
